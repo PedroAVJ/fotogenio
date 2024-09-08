48ae98709fdb77bcd6b5476b459feff3a9d9a6cb
PedroAVJ <109568868+PedroAVJ@users.noreply.github.com>
2024-09-08 22:22:20 +0000

    Add border gradient hack

--- src/app/generar-imagenes/page.tsx
+++ src/app/generar-imagenes/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return 'Generar imagenes';
+  return (
+    <div className="border-gradient-step w-[300px] rounded-[20px] border-x-[5px] border-l-secondary border-r-primary bg-no-repeat p-[40px]">
+      Generar imagenes
+    </div>
+  );
 }
