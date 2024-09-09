3f60f173e29cf1b7bf0fa12b8296ddb515aca9c6
PedroAVJ <109568868+PedroAVJ@users.noreply.github.com>
2024-09-09 21:34:53 +0000

    Update landing page heading to reflect future dashboard purpose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <>
-      <h1>Landing page</h1>
+      <h1>Este sera el dashboard en un futuro</h1>
       <Link href="/generar-imagenes">Generar imagenes</Link>
     </>
   );
