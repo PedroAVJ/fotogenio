c51a769622e6150c452054f8d1424283adb41027
PedroAVJ <109568868+PedroAVJ@users.noreply.github.com>
2024-09-09 15:49:47 +0000

    Update schema paths in drizzle.config to include specific database files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/db/control/schema/*',
+  schema: [
+    './src/server/db/relations.ts',
+    './src/server/db/styles.ts',
+    './src/server/db/users.ts',
+    './src/server/db/chosen-styles.ts',
+  ],
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env['SUPABASE_DB_URL'] ?? '',
