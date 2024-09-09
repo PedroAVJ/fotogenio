bca1904a65b814f4ac111c52b88def270fdfbd01
PedroAVJ <109568868+PedroAVJ@users.noreply.github.com>
2024-09-09 21:01:05 +0000

    Remove comment

--- src/app/generar-imagenes/page.tsx
+++ src/app/generar-imagenes/page.tsx
@@ -1,10 +1,5 @@
 import { ChooseGender } from './choose-gender';
 
 export default function Page() {
-  /* return (
-    <div className="border-gradient-step w-[300px] rounded-[20px] border-x-[5px] border-l-secondary border-r-primary bg-no-repeat p-[40px]">
-      Generar imagenes
-    </div>
-  ); */
   return <ChooseGender />;
 }
