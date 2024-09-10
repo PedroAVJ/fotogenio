c123bb48a9469cc6360618f928bc4f12b2062c56
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-09-10 09:12:34 -0500

    Fix type errors

--- src/app/generar-imagenes/choose-styles.tsx
+++ src/app/generar-imagenes/choose-styles.tsx
@@ -1,20 +1,21 @@
 'use client';
 
+import { Gender } from '@prisma/client';
 import Image from 'next/image';
 import { useLocalStorage } from 'usehooks-ts';
 
 import { Button } from '@/components/ui/button';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { H1, H3 } from '@/components/ui/typography';
-import type { genderChoices } from '@/server/db/styles';
 
 export function ChooseStyles() {
-  const [selectedGender, setSelectedGender] = useLocalStorage<
-    (typeof genderChoices)[number] | null
-  >('gender', null);
+  const [selectedGender, setSelectedGender] = useLocalStorage<Gender | null>(
+    'gender',
+    null,
+  );
   const [currentStep, setCurrentStep] = useLocalStorage('step', 1);
   return (
-    <div className="flex size-full flex-col items-center space-y-20 px-3 pb-12 pt-3">
+    <>
       <div className="flex w-full space-x-2">
         <H1 className="border-gradient-step flex size-16 items-center justify-center rounded-lg border-x-4 border-l-secondary border-r-primary bg-no-repeat font-medium text-primary">
           {currentStep}
@@ -29,15 +30,15 @@ export function ChooseStyles() {
           if (value === '') {
             setSelectedGender(null);
           } else {
-            setSelectedGender(value as (typeof genderChoices)[number]);
+            setSelectedGender(value as Gender);
           }
         }}
-        className="flex w-full grow flex-col items-center justify-evenly space-y-4 md:flex-row"
+        className="flex w-full flex-col items-center justify-evenly space-y-4 md:flex-row"
       >
         <ToggleGroupItem
-          value="male"
+          value={Gender.male}
           aria-label="Toggle male"
-          className="relative h-60 w-72 md:h-72 md:w-96"
+          className="relative h-56 w-72 md:h-72 md:w-96"
         >
           <Image
             src="https://uxsi5qpvaazgwqzm.public.blob.vercel-storage.com/gender-male-k92XCfMROnzg0OlAT39xsiJC1xfm4S.png"
@@ -47,9 +48,9 @@ export function ChooseStyles() {
           />
         </ToggleGroupItem>
         <ToggleGroupItem
-          value="female"
+          value={Gender.female}
           aria-label="Toggle female"
-          className="relative h-60 w-72 md:h-72 md:w-96"
+          className="relative h-56 w-72 md:h-72 md:w-96"
         >
           <Image
             src="https://uxsi5qpvaazgwqzm.public.blob.vercel-storage.com/gender-female-tpgFLyrunZe1K0FK4m0JeEO2Y6evh3.png"
@@ -68,6 +69,6 @@ export function ChooseStyles() {
       >
         Siguiente
       </Button>
-    </div>
+    </>
   );
 }
