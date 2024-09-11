ebb7f2426498bc2a5f3738d9fce2fa6d1458bbc4
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-09-10 21:17:55 -0500

    Change prisma and add script

--- script.ts
+++ script.ts
@@ -3,51 +3,66 @@ import dotenv from 'dotenv';
 import OpenAI from 'openai';
 import { zodResponseFormat } from 'openai/helpers/zod';
 import { z } from 'zod';
+import { db } from '@/server/db';
 
 const Prompt = z.object({
   description: z.string(),
 });
 
-// Load environment variables from .env.local
-dotenv.config({ path: '.env.local' });
+// Load environment variables from .env
+dotenv.config({ path: '.env' });
 
 const openai = new OpenAI();
 
 const main = async () => {
-  const images = await list({
-    prefix: 'male/',
+  const parent = await list({
+    prefix: 'female/',
+    mode: 'folded', 
   });
 
-  // Always skip the first image
-  // eslint-disable-next-line no-restricted-syntax
-  for (const blob of images.blobs.slice(3, 4)) {
-    console.log(blob.url);
-    // eslint-disable-next-line no-await-in-loop
-    const completion = await openai.beta.chat.completions.parse({
-      model: 'gpt-4o-2024-08-06',
-      messages: [
-        {
-          role: 'system',
-          content:
-            'You are an image labeler for an Image Generation Model. You must describe the image in such a way that you refer to the person in the image as the "TOK". This is to trigger the fine tuning. For example: "TOK posing with a sword in a desert", "TOK in a cyberpunk city". Keep short and to one sentence.',
-        },
-        {
-          role: 'user',
-          content: [
-            {
-              type: 'image_url',
-              image_url: {
-                url: blob.url,
-              },
-            },
-          ],
-        },
-      ],
-      response_format: zodResponseFormat(Prompt, 'prompt'),
+  for (const folder of parent.folders) {
+    const images = await list({
+      prefix: folder,
     });
-
-    const event = completion.choices[0]?.message.parsed;
-    console.log(event);
+    const style = await db.style.create({
+      data: {
+        coverPhotoUrl: `https://uxsi5qpvaazgwqzm.public.blob.vercel-storage.com/${folder}gender-female-tpgFLyrunZe1K0FK4m0JeEO2Y6evh3.png`,
+        description: `female-${folder}`,
+        gender: 'female',
+      },
+    });
+    for (const blob of images.blobs.slice(1)) {
+      const completion = await openai.beta.chat.completions.parse({
+        model: 'gpt-4o-2024-08-06',
+        messages: [
+          {
+            role: 'system',
+            content:
+              'You are an image labeler for an Image Generation Model. You must describe the image in such a way that you refer to the person in the image as the "TOK". This is to trigger the fine tuning. For example: "TOK posing with a sword in a desert", "TOK in a cyberpunk city". Keep short and to one sentence.',
+          },
+          {
+            role: 'user',
+            content: [
+              {
+                type: 'image_url',
+                image_url: {
+                  url: blob.url,
+                },
+              },
+            ],
+          },
+        ],
+        response_format: zodResponseFormat(Prompt, 'prompt'),
+      });
+      const prompt = completion.choices[0]?.message.parsed?.description ?? '';
+      await db.prompt.create({
+        data: {
+          inpaintPhotoUrl: blob.url,
+          prompt,
+          styleId: style.id,
+        },
+      });
+    }
   }
 };
 
