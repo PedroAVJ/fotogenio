cb887ece52c6a143987dabd2786827f3df52d52a
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-09-12 16:31:16 -0500

    Add clerk button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Page() {
   return (
     <>
       <h1>Este sera el dashboard en un futuro</h1>
+      <UserButton />
       <Link href="/generar-imagenes">Generar imagenes</Link>
     </>
   );
