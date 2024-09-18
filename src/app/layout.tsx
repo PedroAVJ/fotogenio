46c7bf74997fec5b738f2458d3773c0cf69b82e0
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-09-18 21:18:27 +0000

    Refactor root page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,13 @@ import '@/lib/global.css';
 
 import { esMX } from '@clerk/localizations';
 import { ClerkProvider } from '@clerk/nextjs';
-import { Work_Sans } from 'next/font/google';
 import type { ReactNode } from 'react';
 
 import { QueryClientProvider } from './query-client-provider';
 
-const workSans = Work_Sans({ subsets: ['latin'] });
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={`${workSans.className}`}
-      suppressHydrationWarning
-    >
+    <html lang="es-MX">
       <ClerkProvider localization={esMX}>
         <body className="h-dvh w-dvw">
           <QueryClientProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link';
-import { UserButton, SignIn } from '@clerk/nextjs';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Page() {
   return (
     <>
-      <h1>Este sera el dashboard en un futuro</h1>
+      <h1>Este sera el landing page en un futuro</h1>
       <UserButton />
-      <SignIn />
       <Link href="/generar-imagenes">Generar imagenes</Link>
     </>
   );
