792b7fdbbb27ccb40ff1b0229317dbd4d148bddc
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-09-18 10:09:56 -0500

    Add sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
-import { UserButton } from '@clerk/nextjs';
+import { UserButton, SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
     <>
       <h1>Este sera el dashboard en un futuro</h1>
       <UserButton />
+      <SignIn />
       <Link href="/generar-imagenes">Generar imagenes</Link>
     </>
   );
