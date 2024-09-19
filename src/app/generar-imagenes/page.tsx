fd9360eb0c778df19ae2f4bb9d4083bf2abcf321
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-09-19 02:52:24 +0000

    Add placeholder pages

--- src/app/generar-imagenes/page.tsx
+++ src/app/generar-imagenes/page.tsx
@@ -1,12 +1,24 @@
 import { db } from '@/server/db';
 import { translateZodErrors } from '@/lib/es-zod';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
 import { Stepper } from './stepper';
 
-export const revalidate = 10_000_000;
-
 export default async function Page() {
+  const { userId } = auth().protect();
   await translateZodErrors();
+  const hasModel = await db.userSettings.findFirst({
+    where: {
+      userId,
+    },
+    select: {
+      hasModel: true,
+    },
+  });
+  if (!hasModel) {
+    redirect('/home');
+  }
   const maleStyles = await db.style.findMany({
     where: {
       gender: 'male',
