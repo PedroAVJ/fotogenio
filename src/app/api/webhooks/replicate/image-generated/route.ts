5473835b32c5a1bdee623bd1f666246da3ce2ebc
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-09-20 15:23:43 +0000

    Finish image generated webhook

--- src/app/api/webhooks/replicate/image-generated/route.ts
+++ src/app/api/webhooks/replicate/image-generated/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { env } from '@/lib/env';
 import { db } from '@/server/db';
-import { replicate } from '@/server/replicate';
 import { validateWebhook } from "replicate";
 
 export async function POST(request: NextRequest) {
@@ -21,49 +20,30 @@ export async function POST(request: NextRequest) {
   await db.generatedPhoto.findUniqueOrThrow({
     where: { userId_promptId: { userId, promptId } },
   });
+  interface Body {
+    output: string[];
+  }
+  const body = await request.json() as Body;
+  const photoUrl = body.output[0];
+  if (!photoUrl) {
+    return NextResponse.json({ error: 'Missing photoUrl' }, { status: 400 });
+  }
+  await db.generatedPhoto.update({
+    where: { userId_promptId: { userId, promptId } },
+    data: { photoUrl },
+  });
   await db.userSettings.update({
-    where: { userId },
-    data: { modelStatus: 'ready' },
-  });
-  const prompts = await db.prompt.findMany({
-    include: {
-      style: {
-        include: {
-          chosenStyles: {
-            where: {
-              userId,
-            },
-          },
-        },
-      },
+    where: {
+      userId,
     },
-  });
-  const modelName = `flux-${userId}`;
-  const baseUrl = env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : env.WEBHOOK_MOCK_URL;
-  await Promise.all(prompts.map(async (prompt) => {
-    await replicate.run(
-      `${env.REPLICATE_OWNER}/${modelName}`,
-      {
-        webhook: `${baseUrl}/api/webhooks/replicate/image-generated?userId=${userId}&promptId=${prompt.id}`,
-        webhook_events_filter: ['completed'],
-        input: {
-          prompt: prompt.prompt,
-          image: prompt.inpaintPhotoUrl,
-          model: "dev",
-          lora_scale: 1,
-          num_outputs: 1,
-          aspect_ratio: "1:1",
-          output_format: "webp",
-          guidance_scale: 3.5,
-          output_quality: 90,
-          prompt_strength: 0.8,
-          extra_lora_scale: 1,
-          num_inference_steps: 28
-        }
+    data: {
+      credits: {
+        decrement: 1,
+      },
+      pendingPhotos: {
+        decrement: 1,
       }
-    );
-  }))
+    }
+  })
   return NextResponse.json({ received: true });
 }
