a79dd65ff1ece8126000a7c4e1a44f1da1bcccd6
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-09-21 02:13:37 +0000

    Do redirects

--- src/app/waiting/page.tsx
+++ src/app/waiting/page.tsx
@@ -1,5 +1,20 @@
 import { WaitingComponent } from "@/components/waiting";
+import { db } from "@/server/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const { userId } = auth().protect();
+  const { modelStatus } = await db.userSettings.findFirstOrThrow({
+    where: {
+      userId,
+    },
+    select: {
+      modelStatus: true,
+    },
+  });
+  if (modelStatus === 'ready') {
+    redirect('/home');
+  }
   return <WaitingComponent />;
 }
