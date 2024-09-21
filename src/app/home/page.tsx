1df8f8e5dcb2f6a925016bc5486384af8b1e575b
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-09-21 16:01:50 +0000

    Add home component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>Home</div>;
+import { HomeComponent } from "@/components/home";
+import { db } from "@/server/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+export default async function Home() {
+  const { userId } = auth().protect();
+  const { modelStatus } = await db.userSettings.findUniqueOrThrow({
+    where: { userId },
+    select: { modelStatus: true },
+  });
+  if (modelStatus === "pending") {
+    redirect("/generar-imagenes");
+  } else if (modelStatus === "training") {
+    redirect("/waiting");
+  }
+  return <HomeComponent />;
 }
