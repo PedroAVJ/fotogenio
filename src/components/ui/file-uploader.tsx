652231e437fc98d7661d744d476c53fff574b564
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-10-02 19:01:34 +0000

    Change stuff

--- src/components/ui/file-uploader.tsx
+++ src/components/ui/file-uploader.tsx
@@ -153,7 +153,6 @@ export function FileUploader(props: FileUploaderProps) {
         toast.promise(onUpload(updatedFiles), {
           loading: `Subiendo ${target}...`,
           success: () => {
-            setFiles([])
             return updatedFiles.length > 0 ? `${target} subidos` : `subido`
           },
           error: `Error al subir ${target}`,
