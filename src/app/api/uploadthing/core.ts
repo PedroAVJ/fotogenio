0f2b747ac384c40d293dd6492805abfb8e77aece
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-10-11 04:07:39 +0000

    Remove unused router

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -5,14 +5,6 @@ import { db } from "@/server/db";
 const f = createUploadthing();
 
 export const fileRouter = {
-  subirFotos: f({ image: { maxFileSize: "8MB", maxFileCount: 20 } })
-    .middleware(async () => {
-      auth().protect();
-      return {};
-    })
-    .onUploadComplete(async () => {
-      return {};
-    }),
   subirZip: f({ "application/zip": { maxFileSize: "256MB", maxFileCount: 1 } })
     .middleware(async () => {
       const { userId } = auth().protect();
