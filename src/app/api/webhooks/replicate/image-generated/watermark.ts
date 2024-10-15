5a27cfc783e6483f4656deebed1c3c34999dfdcd
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-10-15 03:27:42 +0000

    Remove comments

--- src/app/api/webhooks/replicate/image-generated/watermark.ts
+++ src/app/api/webhooks/replicate/image-generated/watermark.ts
@@ -4,31 +4,21 @@ import path from 'path';
 import { FileEsque } from "uploadthing/types";
 
 export async function addWatermark(imageUrl: string): Promise<FileEsque> {
-  // Fetch the input image from the provided URL
   const imageResponse = await fetch(imageUrl);
   const imageBuffer = await imageResponse.arrayBuffer();
   const image = sharp(Buffer.from(imageBuffer));
-  
   const metadata = await image.metadata();
   const width = metadata.width || 0;
   const height = metadata.height || 0;
-  
   const watermarkPath = path.join(process.cwd(), 'src/app/api/webhooks/replicate/image-generated', 'watermark.png');
   const watermarkBuffer = await readFile(watermarkPath);
-  
-  // Load the watermark image
   const watermark = sharp(Buffer.from(watermarkBuffer));
   const watermarkMetadata = await watermark.metadata();
-  
-  // Resize watermark if it's too large (e.g., max 25% of the main image size)
   const maxWatermarkWidth = Math.floor(width * 0.25);
   const maxWatermarkHeight = Math.floor(height * 0.25);
-  
   if ((watermarkMetadata.width || 0) > maxWatermarkWidth || (watermarkMetadata.height || 0) > maxWatermarkHeight) {
     watermark.resize(maxWatermarkWidth, maxWatermarkHeight, { fit: 'inside' });
   }
-  
-  // Make the watermark semi-transparent
   const transparentWatermark = await watermark
     .composite([{
       input: Buffer.from([255, 255, 255, 128]),
@@ -37,20 +27,12 @@ export async function addWatermark(imageUrl: string): Promise<FileEsque> {
       blend: 'dest-in'
     }])
     .toBuffer();
-  
   image
     .composite([
       { input: transparentWatermark, gravity: 'southeast' }
     ])
-
-  // Convert Sharp image to Buffer
   const watermarkedImageBuffer = await image.toBuffer();
-
-  // Create a Blob from the Buffer
-  const watermarkedBlob = new Blob([watermarkedImageBuffer], { type: 'image/webp' }); // Adjust the MIME type if necessary
-
-  // Create a File from the Blob
+  const watermarkedBlob = new Blob([watermarkedImageBuffer], { type: 'image/webp' });
   const file = new File([watermarkedBlob], 'watermarked-image.webp', { type: 'image/webp' });
-
   return file;
 }
