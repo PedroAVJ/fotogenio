6193c997d190ae8b70e3376423e1db857e450121
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-10-15 03:41:13 +0000

    Fix code

--- src/app/api/webhooks/replicate/image-generated/watermark.ts
+++ src/app/api/webhooks/replicate/image-generated/watermark.ts
@@ -6,19 +6,11 @@ import { FileEsque } from "uploadthing/types";
 export async function addWatermark(imageUrl: string): Promise<FileEsque> {
   const imageResponse = await fetch(imageUrl);
   const imageBuffer = await imageResponse.arrayBuffer();
-  const image = sharp(Buffer.from(imageBuffer));
-  const metadata = await image.metadata();
-  const width = metadata.width || 0;
-  const height = metadata.height || 0;
-  const watermarkPath = path.join(process.cwd(), 'src/app/api/webhooks/replicate/image-generated', 'watermark.png');
+  const image = sharp(imageBuffer);
+  const basePath = 'src/app/api/webhooks/replicate/image-generated'
+  const watermarkPath = path.join(process.cwd(), basePath , 'watermark.png');
   const watermarkBuffer = await readFile(watermarkPath);
-  const watermark = sharp(Buffer.from(watermarkBuffer));
-  const watermarkMetadata = await watermark.metadata();
-  const maxWatermarkWidth = Math.floor(width * 0.25);
-  const maxWatermarkHeight = Math.floor(height * 0.25);
-  if ((watermarkMetadata.width || 0) > maxWatermarkWidth || (watermarkMetadata.height || 0) > maxWatermarkHeight) {
-    watermark.resize(maxWatermarkWidth, maxWatermarkHeight, { fit: 'inside' });
-  }
+  const watermark = sharp(watermarkBuffer);
   const transparentWatermark = await watermark
     .composite([{
       input: Buffer.from([255, 255, 255, 128]),
@@ -33,6 +25,5 @@ export async function addWatermark(imageUrl: string): Promise<FileEsque> {
     ])
   const watermarkedImageBuffer = await image.toBuffer();
   const watermarkedBlob = new Blob([watermarkedImageBuffer], { type: 'image/webp' });
-  const file = new File([watermarkedBlob], 'watermarked-image.webp', { type: 'image/webp' });
-  return file;
+  return new File([watermarkedBlob], 'watermarked-image.webp', { type: 'image/webp' });
 }
