40f4373213843b7a3936b25fe3197137b320061c
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-10-16 21:52:37 +0000

    Refactor webhook

--- src/app/api/webhooks/replicate/image-generated/route.ts
+++ src/app/api/webhooks/replicate/image-generated/route.ts
@@ -3,7 +3,6 @@ import { env } from '@/server/env';
 import { db } from '@/server/db';
 import { validateWebhook } from "replicate";
 import { utapi } from "@/server/uploadthing";
-import * as Sentry from "@sentry/nextjs";
 import { Prediction } from 'replicate';
 import { addWatermark } from './watermark';
 import fs from 'fs/promises';
@@ -13,60 +12,23 @@ export async function POST(request: NextRequest) {
   const requestClone = request.clone();
   const isValid = validateWebhook(requestClone, env.REPLICATE_WEBHOOK_SECRET);
   if (!isValid) {
-    const errorMessage = 'Invalid webhook secret';
-    Sentry.captureMessage(errorMessage, 'error');
-    console.error(errorMessage);
-    return NextResponse.json({ message: errorMessage });
+    throw new Error('Invalid webhook secret');
   }
   const { searchParams } = new URL(request.url);
-  const generatedPhotoId = searchParams.get('generatedPhotoId');
-  if (!generatedPhotoId) {
-    const errorMessage = 'Missing generatedPhotoId';
-    Sentry.captureMessage(errorMessage, 'error');
-    console.error(errorMessage);
-    return NextResponse.json({ message: errorMessage });
-  }
-  const { status, output } = await request.json() as Prediction;
-  if (status !== 'succeeded') {
-    const errorMessage = `Expected status: succeeded, found: ${status}`;
-    Sentry.captureMessage(errorMessage, 'error');
-    console.error(errorMessage);
-    return NextResponse.json({ message: errorMessage });
-  }
-  if (!Array.isArray(output) || !output.every(item => typeof item === 'string')) {
-    const errorMessage = `Expected output to be an array of strings, found: ${output}`;
-    Sentry.captureMessage(errorMessage, 'error');
-    console.error(errorMessage);
-    return NextResponse.json({ message: errorMessage });
-  }
-  const photoUrl = output[0];
-  if (!photoUrl) {
-    const errorMessage = 'Missing photoUrl';
-    Sentry.captureMessage(errorMessage, 'error');
-    console.error(errorMessage);
-    return NextResponse.json({ message: errorMessage });
-  }
-  const file = await addWatermark(photoUrl);
+  const generatedPhotoId = searchParams.get('generatedPhotoId') ?? '';
+  const { output } = await request.json() as Prediction;
+  const generatedPhotoUrl = output[0] as string;
+  const file = await addWatermark(generatedPhotoUrl);
   const uploadedFiles = await utapi.uploadFiles([file]);
-  const photoUrlWithWatermark = uploadedFiles[0]?.data?.appUrl;
-  if (!photoUrlWithWatermark) {
-    const errorMessage = `Failed to upload watermarked image: ${uploadedFiles}`;
-    Sentry.captureMessage(errorMessage, 'error');
-    console.error(errorMessage);
-    return NextResponse.json({ message: errorMessage });
-  }
-  const generatedPhoto = await db.generatedPhoto.findFirst({
+  const photoUrlWithWatermark = uploadedFiles[0]?.data?.appUrl ?? '';
+  const { id, userId, photoUrl } = await db.generatedPhoto.findUniqueOrThrow({
     where: {
       id: generatedPhotoId,
     },
   });
-  if (!generatedPhoto) {
-    const errorMessage = 'Generated photo already exists';
-    Sentry.captureMessage(errorMessage, 'error');
-    console.error(errorMessage);
-    return NextResponse.json({ message: errorMessage });
+  if (photoUrl) {
+    return NextResponse.json({ message: 'Webhook retry catched' });
   }
-  const { id, userId } = generatedPhoto;
   if (env.NODE_ENV === 'test') {
     const response = await fetch(photoUrlWithWatermark);
     const arrayBuffer = await response.arrayBuffer();
@@ -74,9 +36,7 @@ export async function POST(request: NextRequest) {
     const testFileName = 'generated-image.test.webp'
     const expectedImagePath = path.join(process.cwd(), basePath, testFileName)
     await fs.writeFile(expectedImagePath, Buffer.from(arrayBuffer))
-    const message = 'Test webhook received.';
-    console.log(message);
-    return NextResponse.json({ message });
+    return NextResponse.json({ message: 'Test webhook received' });
   }
   await db.generatedPhoto.update({
     where: { id },
@@ -90,5 +50,5 @@ export async function POST(request: NextRequest) {
       },
     }
   });
-  return NextResponse.json({ received: true });
+  return NextResponse.json({ message: 'Webhook received' });
 }
