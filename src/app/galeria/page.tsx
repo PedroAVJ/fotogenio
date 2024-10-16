ff92439b9c2fac5af4fbd8ea2de85a9fa571d8d0
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-10-16 22:21:17 +0000

    Fix type errors

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,17 +1,12 @@
 import { HomeComponent } from "./home";
 import { db } from "@/server/db";
 import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
   const { userId } = auth().protect();
-  const userSettings = await db.userSettings.findUnique({
+  const { credits } = await db.userSettings.findUniqueOrThrow({
     where: { userId },
   });
-  if (!userSettings) {
-    redirect('/registrarse/selecciona-genero')
-  }
-  const { credits } = userSettings;
   const generatedPhotos = await db.generatedPhoto.findMany({
     where: { userId, photoUrl: { not: null } },
     orderBy: { createdAt: "desc" },
