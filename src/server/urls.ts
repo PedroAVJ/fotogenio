dbd392dc8a16b72e2c5dc4c769d8f8774db63a92
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-10-17 02:44:17 +0000

    Fix stuff

--- src/server/urls.ts
+++ src/server/urls.ts
@@ -4,10 +4,10 @@ let baseUrl: string;
 let webhookBaseUrl: string;
 
 if (env.VERCEL_ENV === 'production') {
-  const baseUrl = `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  baseUrl = `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`;
   webhookBaseUrl = `${baseUrl}/api/webhooks`;
 } else if (env.VERCEL_ENV === 'preview') {
-  const baseUrl = `https://${env.VERCEL_BRANCH_URL}`;
+  baseUrl = `https://${env.VERCEL_BRANCH_URL}`;
   webhookBaseUrl = `${baseUrl}/api/webhooks`;
 } else {
   baseUrl = 'http://localhost:3000';
