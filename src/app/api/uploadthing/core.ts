0bcf859ac3e82c9135e053a05fca812f57b10654
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2024-10-27 01:49:39 +0000

    Catch uploadthing errors

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,15 +3,24 @@ import {
   type FileRouter as UploadThingFileRouter,
 } from "uploadthing/next";
 import { captionImage } from "./caption-image";
+import * as Sentry from "@sentry/nextjs";
 
 const f = createUploadthing();
 
 export const fileRouter = {
   subirZip: f({
     "application/zip": { maxFileSize: "256MB", maxFileCount: 1 },
-  }).onUploadComplete(() => {
-    return {};
-  }),
+  })
+    .onUploadError(({ error, fileKey }) => {
+      Sentry.captureException(error, {
+        extra: {
+          fileKey,
+        },
+      });
+    })
+    .onUploadComplete(() => {
+      return {};
+    }),
   subirFotos: f(
     {
       image: { maxFileSize: "8MB", maxFileCount: 20 },
@@ -19,12 +28,20 @@ export const fileRouter = {
     {
       awaitServerData: true,
     },
-  ).onUploadComplete(async ({ file: { appUrl } }) => {
-    const caption = await captionImage(appUrl);
-    return {
-      caption,
-    };
-  }),
+  )
+    .onUploadError(({ error, fileKey }) => {
+      Sentry.captureException(error, {
+        extra: {
+          fileKey,
+        },
+      });
+    })
+    .onUploadComplete(async ({ file: { appUrl } }) => {
+      const caption = await captionImage(appUrl);
+      return {
+        caption,
+      };
+    }),
 } satisfies UploadThingFileRouter;
 
 export type FileRouter = typeof fileRouter;
